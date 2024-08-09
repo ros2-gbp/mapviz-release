55019bec98b426d529c029e996d4fa46d4087e8b
David Anthony <david.anthony@swri.org>
2024-08-09 09:46:49 -0500

    Rebase from 'upstream'

--- mapviz/include/mapviz/map_canvas.h
+++ mapviz/include/mapviz/map_canvas.h
@@ -40,8 +40,8 @@
 // ROS libraries
 #include <rclcpp/rclcpp.hpp>
 #include <tf2/transform_datatypes.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include <mapviz/mapviz_plugin.h>
 
