a45a9853a35ffdf1e4f87f8db93510db4dbc7c4a
david.anthony@swri.org <david.anthony@swri.org>
2026-08-12 21:45:44 -0500

    Rebase from 'release/kilted/mapviz_plugins'

--- src/pose_plugin.cpp
+++ src/pose_plugin.cpp
@@ -103,7 +103,7 @@ namespace mapviz_plugins
   void PosePlugin::SelectTopic()
   {
     auto [topic, qos] = SelectTopicDialog::selectTopic(
-      node_,
+      TopicSource(),
       "geometry_msgs/msg/PoseStamped",
       qos_);
   
@@ -135,17 +135,23 @@ namespace mapviz_plugins
       topic_ = topic;
       if (!topic.empty())
       {
-        pose_sub_ = node_->create_subscription<geometry_msgs::msg::PoseStamped>(
-          topic_,
-          rclcpp::QoS(1),
-          std::bind(&PosePlugin::PoseCallback, this, std::placeholders::_1));
+        // Preserve the previous fixed depth-1 QoS while routing delivery
+        // through Subscribe(), which hands each message to handlePose() on the
+        // GUI thread where plugin state may be touched without locking.
+        rmw_qos_profile_t pose_qos = rmw_qos_profile_default;
+        pose_qos.depth = 1;
+        Subscribe<geometry_msgs::msg::PoseStamped>(
+          topic_, pose_qos, pose_sub_,
+          [this](geometry_msgs::msg::PoseStamped::ConstSharedPtr msg) {
+            handlePose(msg);
+          });
 
-        RCLCPP_INFO(node_->get_logger(), "Subscribing to %s", topic_.c_str());
+        RCLCPP_INFO(Logger(), "Subscribing to %s", topic_.c_str());
       }
     }
   }
 
-  void PosePlugin::PoseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr pose)
+  void PosePlugin::handlePose(const geometry_msgs::msg::PoseStamped::ConstSharedPtr pose)
   {
     if (!has_message_)
     {
