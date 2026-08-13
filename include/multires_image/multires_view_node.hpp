eee8dc85c908ef18e4cb8b9bf1a6140f255ff5a9
david.anthony@swri.org <david.anthony@swri.org>
2026-08-12 21:40:24 -0500

    Rebase from 'rpm/humble/multires_image'

--- include/multires_image/multires_view_node.hpp
+++ include/multires_image/multires_view_node.hpp
@@ -27,61 +27,57 @@
 //
 // *****************************************************************************
 
-#ifndef MULTIRES_IMAGE_TILE_SET_LAYER_H_
-#define MULTIRES_IMAGE_TILE_SET_LAYER_H_
+#ifndef MULTIRES_IMAGE_MULTIRES_VIEW_NODE_HPP_
+#define MULTIRES_IMAGE_MULTIRES_VIEW_NODE_HPP_
 
 // C++ standard libraries
 #include <string>
-#include <vector>
+#include <thread>
 
-#include <tf2/transform_datatypes.hpp>
+// QT libraries
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QLabel>
+#include <QShowEvent>
 
-#include <swri_transform_util/georeference.h>
+// ROS libraries
+#include <rclcpp/rclcpp.hpp>
 
-#include <multires_image/tile.h>
+#include <multires_image/QGLMap.hpp>
+#include <multires_image/tile_set.hpp>
 
 namespace multires_image
 {
-  class TileSetLayer
+  class MultiresViewNode : public QMainWindow
   {
+    Q_OBJECT
+
   public:
-    TileSetLayer(
-      const swri_transform_util::GeoReference& geo,
-      const std::string& path,
-      int tileSize, int layer);
+    MultiresViewNode(int argc, char **argv, QWidget *parent = 0, Qt::WindowFlags flags = Qt::WindowFlags());
+    ~MultiresViewNode() override = default;
 
-    ~TileSetLayer() = default;
+    virtual void showEvent(QShowEvent* event) override;
 
-    bool Load();
-    bool Load(const std::string extension);
+    void Initialize();
 
-    Tile* GetTile(int column, int row) { return m_tiles[column][row]; }
-
-    void GetTileIndex(const tf2::Vector3& position, int& row, int& column) const;
-    void GetTileIndex(double x, double y, int& row, int& column) const;
-    void GetTileRange(
-      const tf2::Vector3& top_left,
-      const tf2::Vector3& bottom_right,
-      int& startRow, int& startColumn,
-      int& endRow, int& endColumn) const;
-
-    int RowCount() { return m_rows; }
-    int ColumnCount() { return m_columns; }
+    void Spin();
 
   private:
-    const swri_transform_util::GeoReference& m_geo;
-    const std::string      m_path;
-    const int              m_tileSize;
-    const int              m_layer;
-    const double           m_scale;
+    void SpinLoop();
 
-    bool                   m_expectTiles;
+    int argc_;
+    char** argv_;
 
-    int                    m_columns;
-    int                    m_rows;
+    rclcpp::Node::SharedPtr node_;
+    rclcpp::executors::SingleThreadedExecutor executor_;
+    std::thread*  thread_;
 
-    std::vector<std::vector<Tile*> > m_tiles;
+    bool initialized_;
+
+    std::string image_path_;
+
+    TileSet* tile_set_;
   };
 }
 
-#endif  // MULTIRES_IMAGE_TILE_SET_LAYER_H_
+#endif  // MULTIRES_IMAGE_MULTIRES_VIEW_NODE_HPP_
